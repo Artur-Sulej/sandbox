c6a444e67d2ba20c524bd5e16cf508b229ce1063
Artur Sulej <artur@shedul.com>
2022-07-09 18:49:45 +0200

    Generated a new project with command: mix phx.new sandbox --no-html --no-assets --no-ecto --no-gettext --no-mailer

---
+++ config/test.exs
@@ -0,0 +1,14 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :sandbox, SandboxWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "i0kGy5PF+PvkIeUqoZR+gBmLa8OqGQuCWuPvHr+0F0t5UKuc7NQq75rsxrM+6mo/",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
